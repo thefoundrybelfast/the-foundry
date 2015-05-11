ca2692e4033bc0236fe32bcb422fd13dd83f0111
theshawe <david@invisiblebuilding.com>
2015-05-11 03:20:48 +0100

    base url +gulp

--- _site/js/main.js
+++ _site/js/main.js
@@ -7,6 +7,8 @@ $('a').click(function(){
 
 new WOW().init();
 
+
+
 $('.nav a').on('click', function(){
     $(".btn-navbar").click(); //bootstrap 2.x
     $(".navbar-toggle").click() //bootstrap 3.x by Richard

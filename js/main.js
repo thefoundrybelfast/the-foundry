183388b78058c4e14065575b2ae32c6be18c88e3
theshawe <david@invisiblebuilding.com>
2015-05-11 03:07:40 +0100

    js fixes

--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ $('a').click(function(){
 
 new WOW().init();
 
-
-
 $('.nav a').on('click', function(){
     $(".btn-navbar").click(); //bootstrap 2.x
     $(".navbar-toggle").click() //bootstrap 3.x by Richard
